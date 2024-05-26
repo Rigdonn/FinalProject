793291ae593cd9889f1697b172e59e0116b4ded1
Alejandro Ruiz Rios <142677603+Rigdonn@users.noreply.github.com>
2024-05-26 22:47:36 +0200

    last commit

--- FinalProject/src/main/java/com/example/finalproject/ViewManageAccounts.java
+++ FinalProject/src/main/java/com/example/finalproject/ViewManageAccounts.java
@@ -1,7 +1,11 @@
 package com.example.finalproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+
+import java.io.IOException;
 
 public class ViewManageAccounts {
     @FXML
@@ -14,4 +18,12 @@ public class ViewManageAccounts {
     private TableColumn tableViewEmail;
     @FXML
     private TableColumn tableViewAddres;
+    @FXML
+    private TableColumn tableColumnBack;
+    @FXML
+    private Button backManage;
+    public void backToView1(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("view1.fxml");
+    }
 }
